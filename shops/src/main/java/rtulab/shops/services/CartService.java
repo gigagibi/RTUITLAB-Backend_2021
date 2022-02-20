8645bb246b7e46efb4676612138ffb311d5a0a87
gigagibi <gii01@mail.ru>
2022-02-20 17:25:20 +0300

    finished putGoodInCart method in CartService

--- shops/src/main/java/rtulab/shops/services/CartService.java
+++ shops/src/main/java/rtulab/shops/services/CartService.java
@@ -2,7 +2,6 @@ package rtulab.shops.services;
 
 import com.auth0.jwt.JWT;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import rtulab.shops.models.dto.GoodInCart;
 import rtulab.shops.models.mongoDocuments.Cart;
@@ -51,17 +50,30 @@ public class CartService {
     public Cart putGoodInCart(String token, GoodInCart goodInCart) throws TooManyBoughtGoodsException {
         String username = JWT.decode(token.substring(7)).getSubject();
         Cart cart = cartRepository.getByUsername(username);
-        if(cart==null) {
+        Good good = goodRepository.getById(goodInCart.getGoodId());
+        if(cart!=null) { //if cart exists
+            if(cart.getBoughtGoods().stream().anyMatch(g -> g.getGoodId().equals(goodInCart.getGoodId()))) { //if good is already in cart, increase amount of it and replace good in cart
+                goodInCart.setBoughtAmount(
+                        goodInCart.getBoughtAmount() + cart.getBoughtGoods()
+                        .stream()
+                        .filter(g -> g.getGoodId().equals(goodInCart.getGoodId()))
+                        .findAny()
+                        .orElse(new GoodInCart())
+                        .getBoughtAmount()
+                );
+                int oldIndex = cart.getBoughtGoods().indexOf(cart.getBoughtGoods().stream().filter(g -> g.getGoodId().equals(goodInCart.getGoodId())).findAny().get());
+                cart.getBoughtGoods().set(oldIndex, goodInCart);
+            }
+        }
+        else { //if user doesn't have a cart, creates it and put good
             cart = new Cart();
             cart.setId(Integer.toString(username.toLowerCase().hashCode()));
             cart.setUsername(username);
             cart.setBoughtGoods(new ArrayList<>());
-            cartRepository.save(cart);
-        }
-        Good good = goodRepository.getById(goodInCart.getGoodId());
-        if(good.getAmount() >= goodInCart.getBoughtAmount()) {
             cart.getBoughtGoods().add(goodInCart);
-            return cart;
+        }
+        if(good.getAmount() >= goodInCart.getBoughtAmount()) { //in 3rd case, when user have a cart but there is no good that he's trying to add, its just put good in cart
+            return cartRepository.save(cart);
         }
         else
             throw new TooManyBoughtGoodsException();
